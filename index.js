340b9da5262f6acf4e5d400f84be77c08d0789ae
wangyaxin <wangyaxin@jingyougroup.com>
2020-09-22 17:08:56 +0800

    查询数据并存mongo数据库中

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const cheerio = require('cheerio')
 const request = require('request')
+const {MongoClient, ObjectID} = require('mongodb')
+const dbUrl = 'mongodb://localhost:27017"'
 let pageNum = 1
 let resultList = []
 const getUrl = (pageNum) => `https://movie.douban.com/top250?start=${(pageNum - 1) * 25}&filter=`
@@ -7,14 +9,53 @@ const sendRequest = (url) => {
   request(url, {}, (req, res) => {
     const {body} = res
     const $ = cheerio.load(body, { decodeEntities: false })
-    if ($('.hd').length  <= 0) {
-      return resultList.forEach(item => {console.log(item)})
-    }
-    pageNum++
-    $('.hd').each((_, item) => {
-      resultList.push($(item).find('.title').html())
+    MongoClient.connect(dbUrl, {useUnifiedTopology: true}, (err, db) => {
+      if (err){
+        console.log(err)
+        process.exit(0)
+      }
+      var dbase
+      if ($('.hd').length  <= 0) {
+        db.close()
+        db = null
+        process.exit(0)
+      }
+      if (!dbase) {
+        dbase = db.db('local')
+      }
+      $('.hd').each((index, item) => {
+        let insertData = {
+          "movieName": $(item).find('.title').html(),
+          "rank": (pageNum - 1) * 10 + +index + 1
+        }
+        dbase.collection('test').insertOne(insertData)
+        console.log(insertData, "insertData")
+      })
+      pageNum++
+      // 
+      sendRequest(getUrl(pageNum))
     })
-    sendRequest(getUrl(pageNum))
   })
 }
-sendRequest(getUrl(pageNum))
+
+const queryOneDataByObjectId = id => {
+  MongoClient.connect(dbUrl, {useUnifiedTopology: true}, (err, db) => {
+    if (err) {
+      console.log(err)
+      process.exit(0)
+    }
+    const dbase = db.db('local')
+    dbase.collection('test').find({"_id": ObjectID(id)}).toArray((err, result) => {
+      if (err) {
+        console.log(err)
+        db.close()
+        db = null
+        process.exit(0)
+      }
+      db.close()
+      console.log(result)
+    })
+  })
+}
+queryOneDataByObjectId('5f69bc8640396c249c80ec64')
+// sendRequest(getUrl(pageNum))
